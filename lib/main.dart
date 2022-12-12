8de26c6fc1ddd8bd195a78501387065e1a3e8f9b
Ayush Singh <singhayush886@gmail.com>
2022-12-12 17:57:35 +0530

    add-side-bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shoopingapp/constraints.dart';
+import 'package:shoopingapp/screens/login_Screen.dart';
 import 'package:shoopingapp/screens/product_overview_screen.dart';
 import 'package:shoopingapp/utils/routes.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,10 +15,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
           fontFamily: 'Poppins',
+          primaryColor: kPrimaryColor,
+          textTheme: GoogleFonts.robotoTextTheme(Theme.of(context).textTheme)
           // brightness: Brightness.dark,
-          accentColor: Colors.red,
-          primarySwatch: Colors.deepOrange),
-      home: ProductOverviewScreen(),
+          // accentColor: Colors.red,
+          // primarySwatch: Colors.deepOrange,
+          ),
+      home: LoginPage(),
+      // ProductOverviewScreen(),
       routes: {
         MyRoutes.productoverviewscreen: (context) => ProductOverviewScreen(),
       },
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,3 +1,5 @@
 class MyRoutes {
   static String productoverviewscreen = "/productoverviewscreen";
+  static String loginscreen = "/login";
+  static String signupscreen = "/signup";
 }
